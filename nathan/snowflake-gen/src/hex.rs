8743361dc509756acb34fe32452f1b6798e0d10a
opossalite <werbird10@gmail.com>
2025-11-16 14:14:17 -0800

    Finished affinity generation and hex insertion, need to implement coordinate map updating and block checking

--- nathan/snowflake-gen/src/hex.rs
+++ nathan/snowflake-gen/src/hex.rs
@@ -1,3 +1,9 @@
+use std::collections::{HashMap, HashSet};
+
+/// Gets subtracted for progressing further from the hex core
+const PROGRESSION_SUBTRACTION: usize = 5;
+const BRANCH_MULTIPLIER: f64 = 0.5;
+
 /// Used on all six sides of a Hex
 pub enum HexWrapper {
     Affinity(usize),
@@ -6,6 +12,7 @@ pub enum HexWrapper {
 }
 
 /// Simple enum for simplification
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub enum Direction {
     Top,
     TopLeft,
@@ -13,6 +20,35 @@ pub enum Direction {
     BottomLeft,
     BottomRight,
     Bottom,
+}
+impl Direction {
+    pub fn get_all() -> [Direction; 6] {
+        [Self::Top, Self::TopLeft, Self::TopRight, Self::BottomLeft, Self::BottomRight, Self::Bottom]
+    }
+
+    /// Used for coordinate calculations
+    pub fn to_offset(&self) -> (f64, f64) {
+        match self {
+            Direction::Top => {
+                (0.0, 1.0)
+            },
+            Direction::TopLeft => {
+                (-1.0, 0.5)
+            },
+            Direction::TopRight => {
+                (1.0, 0.5)
+            },
+            Direction::BottomLeft => {
+                (-1.0, -0.5)
+            },
+            Direction::BottomRight => {
+                (1.0, -0.5)
+            },
+            Direction::Bottom => {
+                (0.0, -1.0)
+            },
+        }
+    }
 }
 
 /// Fundamental struct used for a snowflake
@@ -25,6 +61,8 @@ pub struct Hex {
     bottom: HexWrapper,
 }
 impl Hex {
+
+    /// Retrieve the HexWrapper in any given direction
     pub fn retrieve(&mut self, direction: &Direction) -> &mut HexWrapper {
         match direction {
             Direction::Top => &mut self.top,
@@ -35,13 +73,25 @@ impl Hex {
             Direction::Bottom => &mut self.bottom,
         }
     }
+
+    /// Update the HexWrapper in any given direction, returning the previous one
+    pub fn set_direction(&mut self, direction: &Direction, hex_wrapper: HexWrapper) -> HexWrapper {
+        match direction {
+            Direction::Top => std::mem::replace(&mut self.top, hex_wrapper),
+            Direction::TopLeft => std::mem::replace(&mut self.top_left, hex_wrapper),
+            Direction::TopRight => std::mem::replace(&mut self.top_right, hex_wrapper),
+            Direction::BottomLeft => std::mem::replace(&mut self.bottom_left, hex_wrapper),
+            Direction::BottomRight => std::mem::replace(&mut self.bottom_right, hex_wrapper),
+            Direction::Bottom => std::mem::replace(&mut self.bottom, hex_wrapper),
+        }
+    }
 }
 
 /// Stores the core of the snowflake and all its info
 pub struct CoreHex {
     hex: Hex,
     leaves: Vec<Vec<Direction>>,
-    size: usize,
+    coordinate_map: HashMap<(f64, f64), Vec<Direction>>,
 }
 impl CoreHex {
     pub fn new() -> Self {
@@ -56,40 +106,21 @@ impl CoreHex {
         CoreHex {
             hex,
             leaves: vec![],
-            size: 0,
+            coordinate_map: HashMap::new(),
         }
     }
 
     /// Follows the path, returning the Hex and coords at the end location
-    pub fn follow_path(&mut self, path: Vec<Direction>) -> Option<(&mut Hex, f64, f64)> {
+    pub fn follow_path(&mut self, path: &Vec<Direction>) -> Option<(&mut Hex, f64, f64)> {
         let mut hex = &mut self.hex;
         let mut x_coord = 0.0;
         let mut y_coord = 0.0;
         for direction in path.iter() {
-            match direction {
-                Direction::Top => {
-                    y_coord += 1.0;
-                },
-                Direction::TopLeft => {
-                    x_coord -= 1.0;
-                    y_coord += 0.5;
-                },
-                Direction::TopRight => {
-                    x_coord += 1.0;
-                    y_coord += 0.5;
-                },
-                Direction::BottomLeft => {
-                    x_coord -= 1.0;
-                    y_coord -= 0.5;
-                },
-                Direction::BottomRight => {
-                    x_coord += 1.0;
-                    y_coord -= 0.5;
-                },
-                Direction::Bottom => {
-                    y_coord -= 1.0;
-                },
-            }
+            // coordinate tracking
+            let (dx, dy) = direction.to_offset();
+            x_coord += dx;
+            y_coord += dy;
+
             match hex.retrieve(direction) {
                 HexWrapper::Affinity(_) => return None,
                 HexWrapper::Hex(inner) => hex = inner,
@@ -103,32 +134,55 @@ impl CoreHex {
     pub fn check_blocks(&mut self, new_hex_path: Vec<Direction>) -> Option<()> {
         // check all six directions and update both this and other hexes
         // but how to find other hexes? we don't exactly have a universal grid
+        // use the new coordinate_map!
         return Some(());
     }
 
     /// Insert a new hexagon
     pub fn insert(&mut self, source_hex_path: Vec<Direction>, direction: Direction) -> Option<()> {
         // assumption is that the path is not blocked, because it shouldn't be
-        let end_hex = self.follow_path(source_hex_path)?;
-        match end_hex.0.retrieve(&direction) {
+
+        // first generate affinities
+        let (source_hex, source_x, source_y) = self.follow_path(&source_hex_path)?;
+        let mut direction_affinity_map: HashMap<Direction, usize> = HashMap::new();
+        match source_hex.retrieve(&direction) {
             HexWrapper::Affinity(source_affinity) => {
-                //TODO, need to insert new hex and generate its new affinities,
-                //maybe considering the directionality towards root too and distance,
-                //and also find out which sides are blocked
-                // aaand update existing hexes that are now potentially blocked
+                //maybe considering the directionality towards root too if more aesthetic
 
-                // if continuing a pole, continue the source affinity in the same direction
-                // aka if the last direction in the path and the new direction match
+                let mut directions_to_use: HashSet<Direction> = HashSet::from(Direction::get_all());
 
-                // for every step taken in the path, remove x affinity (5 for now)
+                // check to see if we're continuing a pole (no branch hit)
+                if source_hex_path.last().is_some_and(|d| *d == direction) {
+                    direction_affinity_map.insert(direction.clone(), *source_affinity - PROGRESSION_SUBTRACTION);
+                    directions_to_use.remove(&direction);
+                }
 
-                // if not extending a pole, multiply previous affinity by 0.8
-
-                // finally, add 8 or subtract 8 randomly
+                // fill in the rest of the affinities (take the branch hit)
+                directions_to_use.into_iter().for_each(|d| {
+                    direction_affinity_map.insert(d, (*source_affinity as f64 * BRANCH_MULTIPLIER) as usize - PROGRESSION_SUBTRACTION);
+                });
             },
             HexWrapper::Hex(_) => return None,
             HexWrapper::Blocked => return None,
         }
+
+        // create hex and perform the insertion
+        let new_hex = HexWrapper::Hex(Box::new(Hex {
+            top: HexWrapper::Affinity(*direction_affinity_map.get(&Direction::Top)?),
+            top_left: HexWrapper::Affinity(*direction_affinity_map.get(&Direction::TopLeft)?),
+            top_right: HexWrapper::Affinity(*direction_affinity_map.get(&Direction::TopRight)?),
+            bottom_left: HexWrapper::Affinity(*direction_affinity_map.get(&Direction::BottomLeft)?),
+            bottom_right: HexWrapper::Affinity(*direction_affinity_map.get(&Direction::BottomRight)?),
+            bottom: HexWrapper::Affinity(*direction_affinity_map.get(&Direction::Bottom)?),
+        }));
+        source_hex.set_direction(&direction, new_hex);
+
+        // update the coordinate map
+        // AAAAAA FLOATS FUCKING SUCK
+        //self.coordinate_map.insert();
+
+        // update blocks (for this new hex and existing hexes)
+
         return None;
     }
 }
