e290b8f692ade5f3601c190508b20ba5cde0016d
opossalite <werbird10@gmail.com>
2025-11-16 23:47:09 -0800

    Added reminder to use the frontier

--- nathan/snowflake-gen/src/main.rs
+++ nathan/snowflake-gen/src/main.rs
@@ -8,6 +8,16 @@ fn main() {
     println!("Hello, world!");
 }
 
+/*
+TODO
+
+implement the block updating function
+REMEMBER ABOUT THE FRONTIER, WE FORGOT ABOUT THE FRONTIER
+add a function to choose where to insert a new hex (accounting for all the affinities)
+add a loop function which inserts x number of hexes (parameter which we call here in main)
+
+*/
+
 
 
 
